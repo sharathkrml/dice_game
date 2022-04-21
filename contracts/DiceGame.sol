d5725b132d0aff38db85455a62b60003a29bd4ec
sharath V <sharathkrml@gmail.com>
2022-04-21 13:40:27 +0530

    Completed contract

--- contracts/DiceGame.sol
+++ contracts/DiceGame.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
-contract DiceGame {
+contract DiceGame is KeeperCompatibleInterface {
     struct Prediction {
         uint256 value;
         uint256 time;
         address from;
     }
     uint256 public count;
+    uint256 public diceResult;
+    uint256 public standby;
     Prediction[6] public predictions;
     event NewPrediction(Prediction);
+
     function predict(uint256 _value) external {
         require(count < 6, "Cannot add more");
         Prediction memory newPrediction = Prediction(
@@ -23,7 +27,7 @@ contract DiceGame {
         emit NewPrediction(newPrediction);
     }
 
-    function getRankList(uint256 _actualvalue) external {
+    function getRankList(uint256 _actualvalue) internal {
         for (uint256 i = 0; i < predictions.length; i++) {
             if (predictions[i].value >= _actualvalue) {
                 predictions[i].value = predictions[i].value - _actualvalue;
@@ -33,7 +37,6 @@ contract DiceGame {
         }
         for (uint256 i = 0; i < predictions.length; i++) {
             for (uint256 j = 0; j < predictions.length - i - 1; j++) {
-                // if current amt > next amt
                 if (predictions[j].value > predictions[j + 1].value) {
                     Prediction memory temp = predictions[j];
                     predictions[j] = predictions[j + 1];
@@ -50,8 +53,39 @@ contract DiceGame {
             }
         }
     }
-    function resetPredictions() external{
-        count=0;
+
+    function resetPredictions() internal {
+        diceResult = 0;
+        count = 0;
         delete predictions;
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded =
+            (diceResult == 0 && count == 6) ||
+            (diceResult != 0 && block.timestamp > standby);
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        if (diceResult == 0 && count == 6) {
+            diceResult = ((block.timestamp + block.difficulty) % 6) + 1;
+            getRankList(diceResult);
+            standby = block.timestamp + 3 minutes;
+        }
+        if (diceResult != 0 && block.timestamp > standby) {
+            resetPredictions();
+        }
     }
 }
